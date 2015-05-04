38c87dae301cb616b4688453c767675041f58d2e
m.leonette.da.cruz <m.leonette.da.cruz@W8DJ0-04X5H6J.corp.avanade.org>
2015-05-04 19:58:05 -0300

    4-5-15 - segunda parte

---
+++ Projeto LP3-ejb/src/java/Model/Usuario.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author m.leonette.da.cruz
+ */
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+public class Usuario implements Serializable{
+    @Id
+    private int usuario_id;
+    private String nome,cpf,cep,numero,cidade,estado, endereco, email, senha;
+
+    public Usuario(String nome, String cpf, String cep, String numero, String cidade, String estado, String endereco, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.endereco = endereco;
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
